e04cd4def03876c12caa4fe6e57b69d587ac3173
joel <joelolguin0@gmail.com>
2020-05-13 13:04:56 -0500

    se importa la base de datos en heroes component.

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../servicios/heroes.service';
+
+
 
 @Component({
   selector: 'app-heroes',
@@ -8,7 +11,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private heroesService: HeroesService) { }
 
   ngOnInit(): void {
   }
